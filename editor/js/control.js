efeb8528736394662fdc6e98c1bb15feb1c3587d
L.S. <79638053+LuciferStarSS@users.noreply.github.com>
2025-12-31 22:15:46 +0800

    Add files via upload

--- editor/js/control.js
+++ editor/js/control.js
@@ -3,7 +3,7 @@
 保存操作将会清除备份。或者，让学生选择是否要清除备份。
 ************************************************************/
 var bSaving = false;			//当前是否在执行保存操作
-var scratch = null;			//Scratch主窗口
+//var scratch = null;			//Scratch2.0 flash版残余数据。
 var bLoaded = false;			//检测是否已经加载项目文件
 var bPlayMode = true;			//Scratch模式：  true:播放模式	false:编辑模式
 var bFirstTimeSaving = true;		//新建后第一次保存，需要刷新文件夹。
