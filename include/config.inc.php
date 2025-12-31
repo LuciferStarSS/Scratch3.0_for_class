e1e16b0de46e3043541025b346ead152bd6dde98
L.S. <79638053+LuciferStarSS@users.noreply.github.com>
2025-12-31 11:39:55 +0800

    Update config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -9,7 +9,7 @@ $WSSERVER	=	($_SERVER['SERVER_ADDR']=="::1")?"127.0.0.1":$_SERVER['SERVER_ADDR']
 *********************************/
 $student_works="student_works/";
 
-$site_path="/scratch3.0_for_class-2/";		//Scratch3.0的editor文件夹所在路径
+$site_path="/scratch3.0_for_class/";		//Scratch3.0的editor文件夹所在路径
                                                 //这个请使用WEB目录中从根目录开始的绝对路径。
 
 
@@ -223,4 +223,5 @@ function validateFilename($filename)
 function validateMsg($msg)
 {
    return str_replace(Array('<script','</script>'),'*',$msg);
+
 }
